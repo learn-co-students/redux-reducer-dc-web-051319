8eec6df4ccaeca4d8031f5506b91f851cd56e7a1
2daysweb <sahnunhm@gmail.com>
2019-08-03 23:09:13 -0400

    passing 5 tests

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
-export function manageFriends(state, action){
+let initialState = {friend: ""}
+
+export function manageFriends(state=initialState, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            console.log("Add friend", action.type)
+            return {}
+        case "REMOVE_FRIEND":
+            console.log("Remove Friend", action.type)
+        default: 
+        return state 
+    }
 }
+
+    // In managePresents.js, 
+    // write a function called managePresents 
+    // that takes in an action and the previous 
+    // state as its argument. Actions passed into 
+    // this reducer only have a type attribute, so 
+    // they would look something like this:
+
+
+
